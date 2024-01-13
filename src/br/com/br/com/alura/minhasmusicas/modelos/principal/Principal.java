51aa8d8fe029b7a0a38a7bccd3bac576aec0dac2
Gabriel Custodio da Silva <gabrielcs225@gmail.com>
2024-01-13 15:58:09 -0300

    origem

---
+++ src/br/com/br/com/alura/minhasmusicas/modelos/principal/Principal.java
@@ -0,0 +1,42 @@
+package br.com.br.com.alura.minhasmusicas.modelos.principal;
+
+import br.com.br.com.alura.minhasmusicas.modelos.MinhasPreferidas;
+import br.com.br.com.alura.minhasmusicas.modelos.Musica;
+import br.com.br.com.alura.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("TNT");
+        minhaMusica.setCantor("AC/DC");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("SpaceToday");
+        meuPodcast.setApresentador("Sergio Sacani");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+            
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+
+    }
+
+}
